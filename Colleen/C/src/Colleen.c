4f973b00eada1c9657c1f35e8cdf6e5a38ce10db
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-10-31 16:59:25 +0200

    Colleen C done

---
+++ Colleen/C/src/Colleen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* Comment outside of the program */
+
+void f() {
+    char *code = "#include <stdio.h>%1$c%1$c/* Comment outside of the program */%1$c%1$cvoid f() {%1$c    char *code = %3$c%2$s%3$c;%1$c    printf(code, 10, code, 34);%1$c}%1$c%1$cint main() {%1$c    /* Comment inside main function */%1$c    f();%1$c    return 0;%1$c}%1$c";
+    printf(code, 10, code, 34);
+}
+
+int main() {
+    /* Comment inside main function */
+    f();
+    return 0;
+}
